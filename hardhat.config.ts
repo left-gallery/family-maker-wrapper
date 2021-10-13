0d44d95451fd155333109739c0e4525a0402baba
vrde <agranzot@mailbox.org>
2021-10-14 00:32:41 +0200

    Add mainnet config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -16,12 +16,9 @@ import("./tasks").catch((e) => console.log("Cannot load tasks", e.toString()));
 // import "solidity-coverage";
 
 const INFURA_API_KEY = process.env.INFURA_API_KEY || "";
-const RINKEBY_PRIVATE_KEY =
-  process.env.RINKEBY_PRIVATE_KEY! ||
-  "0xc87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3"; // well known private key
-const KOVAN_PRIVATE_KEY =
-  process.env.KOVAN_PRIVATE_KEY! ||
-  "0xc87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3"; // well known private key
+const MAINNET_PRIVATE_KEY = process.env.MAINNET_PRIVATE_KEY!;
+const RINKEBY_PRIVATE_KEY = process.env.RINKEBY_PRIVATE_KEY!;
+const KOVAN_PRIVATE_KEY = process.env.KOVAN_PRIVATE_KEY!;
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 const COINMARKETCAP_KEY = process.env.COINMARKETCAP_KEY || "";
 
@@ -44,6 +41,10 @@ const config: HardhatUserConfig = {
     hardhat: {},
     localhost: {
       url: "http://127.0.0.1:8545",
+    },
+    mainnet: {
+      url: `https://mainnet.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: [MAINNET_PRIVATE_KEY],
     },
     kovan: {
       url: `https://kovan.infura.io/v3/${INFURA_API_KEY}`,
