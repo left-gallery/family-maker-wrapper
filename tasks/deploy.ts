18d8c75ba8bb7a5c36991594377e2e412df935e3
vrde <agranzot@mailbox.org>
2021-10-14 10:42:17 +0200

    Visualize gas fees before deploying

--- tasks/deploy.ts
+++ tasks/deploy.ts
@@ -1,21 +1,39 @@
+import { formatUnits } from "@ethersproject/units";
 import { task } from "hardhat/config";
 import { FamilyMakerWrapper__factory } from "../typechain";
-import { mergeNetworkArtifact } from "./utils";
+import { confirm, mergeNetworkArtifact } from "./utils";
 
 task("deploy", "Deploy FamilyMakerWrapper")
   .addParam("legacyAddress", "Address of the family maker contract to wrap")
   .setAction(async ({ legacyAddress }, hre) => {
+    const { chainId } = await hre.ethers.provider.getNetwork();
     console.log("Deploy FamilyMakerWrapper, legacy contract:", legacyAddress);
     const wrapperFactory = (await hre.ethers.getContractFactory(
       "FamilyMakerWrapper"
     )) as FamilyMakerWrapper__factory;
 
+    const feeData = await hre.ethers.provider.getFeeData();
+    if (feeData.maxFeePerGas) {
+      console.log("  Max Fee:", formatUnits(feeData.maxFeePerGas, "gwei"));
+    }
+    if (feeData.maxPriorityFeePerGas) {
+      console.log(
+        "  Max Priority Fee:",
+        formatUnits(feeData.maxPriorityFeePerGas, "gwei")
+      );
+    }
+    if (feeData.gasPrice) {
+      console.log("  Gas Price:", formatUnits(feeData.gasPrice, "gwei"));
+    }
+
+    if (!(await confirm("Deploy? (yes/no)"))) {
+      return;
+    }
+
     const wrapperContract = await wrapperFactory.deploy(legacyAddress);
     console.log("  Address", wrapperContract.address);
     const receipt = await wrapperContract.deployed();
     console.log("  Receipt", receipt.deployTransaction.hash);
-
-    const { chainId } = await hre.ethers.provider.getNetwork();
 
     const config = {
       FamilyMakerWrapper: wrapperContract.address,
--- tasks/utils.ts
+++ tasks/utils.ts
@@ -1,8 +1,12 @@
+import readline from "readline";
 import { readFile, writeFile } from "fs/promises";
 import { join } from "path";
+import { mkdir } from "fs/promises";
 
 export async function mergeNetworkArtifact(chainId: number, data: any) {
-  const filename = join(__dirname, "../artifacts/", `${chainId}.json`);
+  const dirpath = join(__dirname, "../deployments/");
+  await mkdir(dirpath, { recursive: true });
+  const filename = join(dirpath, `${chainId}.json`);
   let stored = {};
   try {
     stored = JSON.parse(await readFile(filename, "utf8"));
@@ -13,4 +17,17 @@ export async function mergeNetworkArtifact(chainId: number, data: any) {
     }
   }
   await writeFile(filename, JSON.stringify({ ...stored, ...data }, null, 2));
+}
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+export function confirm(question: string) {
+  return new Promise((resolve) => {
+    rl.question(question + " ", (answer) => {
+      resolve(answer === "yes");
+    });
+  });
 }
